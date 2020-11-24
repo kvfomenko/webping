15b4812c47489f8bae2a9751d542b905511507ec
Konstantin Fomenko <k.fomenko@>
2020-11-24 22:15:03 +0200

    added pwa_sw.js

---
+++ pwa_sw.js
@@ -0,0 +1,31 @@
+var cacheName = 'web-ping-pwa';
+var filesToCache = [
+  '/webping',
+  '/webping/images/icon-128.png',
+  '/webping/images/icon-144.png',
+  '/webping/images/icon-152.png',
+  '/webping/images/icon-192.png',
+  '/webping/images/icon-256.png',
+  '/webping/images/icon-512.png'
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      console.log('request ' + e.request.url + ' matched')
+      return response || fetch(e.request);
+    }).catch(
+        console.log('request ' + e.request.url + ' not matched!!!')
+    )
+  );
+});
